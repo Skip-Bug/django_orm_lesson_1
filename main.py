e7fb569126a38253f53d0a398919e57d148d9072
Leonid Stoyko <gribojor@gmail.com>
2026-03-17 02:40:15 +0500

    removed the loop

--- main.py
+++ main.py
@@ -10,10 +10,5 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    some_cards = (Passcard.objects.all())
-
-    active_passcards = []
-    for some_card in some_cards:
-        if some_card.is_active is True:
-            active_passcards.append(some_card)
+    active_passcards = (Passcard.objects.filter(is_active=True))
     print('Количество активных пропусков:', len(active_passcards))
