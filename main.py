e80ec8a97e0c0222145b04054f5682410f2b8137
Leonid Stoyko <gribojor@gmail.com>
2026-03-17 02:44:30 +0500

    reduced

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    active_passcards = (Passcard.objects.filter(is_active=True))
-    print('Количество активных пропусков:', len(active_passcards))
+    print('Количество активных пропусков:',
+          Passcard.objects.filter(is_active=True).count()
+          )
